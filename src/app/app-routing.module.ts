5635916a85b4d7c95c6a59d48dbf021a9e284743
Puja Singh <puja@Pujas-MacBook-Air.local>
2024-02-13 18:59:49 +0100

    Routing added for Navigating between features

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecepiesComponent } from './recepies/recepies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/recepies', pathMatch: 'full' },
+  { path: 'recepies', component: RecepiesComponent },
+  { path: 'shopoping-list', component: ShoppingListComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
