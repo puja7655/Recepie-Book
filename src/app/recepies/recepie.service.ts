4b03a453ccef9954ebdc82db770fd079c0adc979
Puja Singh <puja@Pujas-MacBook-Air.local>
2024-02-23 10:55:15 +0100

    hardcoding my project since application is not yet deployed on firebase

--- src/app/recepies/recepie.service.ts
+++ src/app/recepies/recepie.service.ts
@@ -8,31 +8,31 @@ import { Subject } from 'rxjs';
 })
 export class RecepieService {
   recepieChanged = new Subject<recepie[]>()
-  // commenting the hardcoded recepie since now firebase database is set up for backend
-  // private recepies: recepie[] = [new recepie(
-  //   'test recepie1', 'description1',
-  //   'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F4551832%2Fpexels-photo-4551832.jpeg',
-  //   [
-  //     (new Ingredients('Meat', 1)),
-  //     (new Ingredients('Bread', 5))
-  //   ]
-  // ),
-  // new recepie('test recepie2', 'description2',
-  //   'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F4551832%2Fpexels-photo-4551832.jpeg',
-  //   [
-  //     (new Ingredients('chicken', 1)),
-  //     (new Ingredients('potato', 5))
-  //   ]
-  // ),
-  // new recepie('test recepie3', 'description3',
-  //   'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F4551832%2Fpexels-photo-4551832.jpeg',
-  //   [
-  //     (new Ingredients('milk', 1)),
-  //     (new Ingredients('Bread', 5))
-  //   ]
-  // )]
+  private recepies: recepie[] = [new recepie(
+    'test recepie1', 'description1',
+    'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F4551832%2Fpexels-photo-4551832.jpeg',
+    [
+      (new Ingredients('Meat', 1)),
+      (new Ingredients('Bread', 5))
+    ]
+  ),
+  new recepie('test recepie2', 'description2',
+    'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F4551832%2Fpexels-photo-4551832.jpeg',
+    [
+      (new Ingredients('chicken', 1)),
+      (new Ingredients('potato', 5))
+    ]
+  ),
+  new recepie('test recepie3', 'description3',
+    'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F4551832%2Fpexels-photo-4551832.jpeg',
+    [
+      (new Ingredients('milk', 1)),
+      (new Ingredients('Bread', 5))
+    ]
+  )]
 
-  private recepies: recepie[] = []
+  // Will uncomment it once i deploy the application on firebase
+  //private recepies: recepie[] = []
 
   constructor() { }
   getRecepie() {
